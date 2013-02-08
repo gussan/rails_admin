83ad27166fdb9d9bd5c2832f2ba01ba25ed3986e
gussan <egussan@gmail.com>
2013-02-08 16:17:55 +0900

    assign_attributes role option is no longer effective

--- lib/rails_admin/adapters/active_record/abstract_object.rb
+++ lib/rails_admin/adapters/active_record/abstract_object.rb
@@ -16,7 +16,11 @@ module RailsAdmin
         end
 
         def set_attributes(attributes, role = nil)
-          object.assign_attributes(attributes, :as => role)
+          if object.method(:assign_attributes).arity > 0
+            object.assign_attributes(attributes)
+          else
+            object.assign_attributes(attributes, :as => role)
+          end
         end
 
         def save(options = { :validate => true })
